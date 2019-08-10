37bdf157eca7baf85158f545b0c428b9090e245e
ionware <ionwarez@gmail.com>
2019-08-10 21:59:13 +0100

    pin verification procedure and changed pin from plain to hash value

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@
 use App\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
@@ -11,7 +12,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'username' => $faker->userName,
         'password' => '$2y$10$QVP.uR2kEtrTL13i6lpIBOB5Sl4kVuReXj1SlLcS3tDjClQPn82hm', // secret
-        'pin' => $faker->numberBetween(1000, 30000),
+        'pin' => Hash::make($faker->numberBetween(1000, 30000)),
         'remember_token' => Str::random(10),
     ];
 });
---
+++ resources/views/auth/pin.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.master')
+@section('title')
+@endsection
+
+@section('content')
+    <!-- Content
+================================================== -->
+    <div id="titlebar" class="gradient">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Verify Login</h2>
+                    <span>Enter PIN to verify identity.</span>
+
+                    <!-- Breadcrumbs -->
+                    <nav id="breadcrumbs" class="white">
+                        <ul>
+                            <li><a href="#">Formend</a></li>
+                            <li>Verify</li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Page Content
+    ================================================== -->
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-5 offset-xl-3">
+
+
+                <div class="login-register-page">
+                    <!-- Welcome Text -->
+                    <div class="welcome-text">
+                        <h3>Enter PIN</h3>
+                        @if($errors->has('authFailed'))
+                            <h4>You entered an incorrect PIN.</h4>
+                        @endif
+                    </div>
+
+                    <!-- Form -->
+                    <form method="post" id="login-form" action="{{ route('pin-verification') }}">
+                        {{ csrf_field() }}
+                        <div class="input-with-icon-left">
+                            <i class="icon-material-outline-lock"></i>
+                            <input type="password" class="input-text with-border" name="pin" id="PIN" placeholder="123456" maxlength="8" required/>
+                        </div>
+                        <a href="#" class="forgot-pin">Recover PIN</a>
+                    </form>
+
+                    <!-- Button -->
+                    <button class="button full-width button-sliding-icon ripple-effect margin-top-10"
+                            type="submit"
+                            form="login-form">
+                        Continue <i class="icon-material-outline-arrow-right-alt"></i>
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Spacer -->
+    <div class="margin-top-70"></div>
+    <!-- Spacer / End-->
+@endsection
