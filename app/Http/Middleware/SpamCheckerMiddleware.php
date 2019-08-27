814e3eee3d20e7b5dbe800b1f4ea90f12c450535
ionware <ionwarez@gmail.com>
2019-08-27 13:47:07 +0100

    updated the spam middleware

--- app/Http/Middleware/SpamCheckerMiddleware.php
+++ app/Http/Middleware/SpamCheckerMiddleware.php
@@ -9,8 +9,7 @@ use Illuminate\Support\Facades\Response;
  * There has been unimportant submissions flooding in from endpoints
  * by Bots. Therefore we set up a kind of custom "Honeypot" where we
  * have an hidden field called "s-verify" that we hope the bot will try to fill,
- * the s-verify will hold the endpoint unique id, and we crosscheck if it matches.
- * Otherwise, a Bot has changed it, and it is a SPAM.
+ * then if s-verify is filled, it is a BOT.
  *
  * Class SpamCheckerMiddleware
  * @package App\Http\Middleware
@@ -26,10 +25,9 @@ class SpamCheckerMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $endpoint = $request->route('endpoint');
-        $formSpamValue = $request->get('s-verify');
-        if ($endpoint !== $formSpamValue)
-            return Response::view('errors.404', [], 404);
+        $formSpamValue = $request->get('s-verify', false);
+        if ($formSpamValue)
+            return abort(404);
 
         return $next($request);
     }
