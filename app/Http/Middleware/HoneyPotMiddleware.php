eef59ffae2961f61d3adab93c92f68adf41d79a4
ionware <ionwarez@gmail.com>
2020-05-04 16:40:21 +0100

    Integrate Google recaptcha for spam filtering
    Re-define the honeypot integration too, custom field for honey pot is now `_gotcha`.

--- app/Http/Middleware/HoneyPotMiddleware.php
+++ app/Http/Middleware/HoneyPotMiddleware.php
@@ -3,30 +3,31 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Response;
+use Illuminate\Http\Request;
 
 /**
  * There has been unimportant submissions flooding in from endpoints
  * by Bots. Therefore we set up a kind of custom "Honeypot" where we
- * have an hidden field called "s-verify" that we hope the bot will try to fill,
- * then if s-verify is filled, it is a BOT.
+ * have an hidden field called "_gotcha" that we hope the bot will try to fill,
+ * then if _gotcha is filled, it is a BOT.
  *
- * Class SpamCheckerMiddleware
+ * Class HoneyPotMiddleware
  * @package App\Http\Middleware
  */
-class SpamCheckerMiddleware
+class HoneyPotMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  Request  $request
+     * @param  Closure  $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        $formSpamValue = $request->get('s-verify', false);
-        if ($formSpamValue)
+        $honeypot_trap = $request->get('_gotcha', false);
+
+        if ($honeypot_trap)
             return abort(404);
 
         return $next($request);
