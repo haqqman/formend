07f1657802c6f81362d88b84cc2af074966c56d2
sulaimanxyz <writeme@sulaiman.xyz>
2020-05-08 08:32:32 +0100

    Updated path of avatar

--- resources/views/layouts/header-auth.blade.php
+++ resources/views/layouts/header-auth.blade.php
@@ -80,7 +80,7 @@
                         <div class="header-notifications-trigger">
                             <a href="#">
                                 <div class="user-avatar status-online">
-                                    <img src="{{ asset('images/haqqman-avatar.jpg') }}" alt="">
+                                    <img src="{{ asset('images/logo-avatar.jpg') }}" alt="">
                                 </div>
                             </a>
                         </div>
@@ -91,7 +91,7 @@
                                 <!-- User Name / Avatar -->
                                 <div class="user-details">
                                     <div class="user-avatar status-online">
-                                        <img src="{{ asset('images/haqqman-avatar.jpg') }}" alt="">
+                                        <img src="{{ asset('images/logo-avatar.jpg') }}" alt="">
                                     </div>
                                     <div class="user-name">
                                         Formend Manager <span>Haqqman Webmaster</span>
